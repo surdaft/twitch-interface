997fd847051ecf53296f0893afa13dc08178ef44
SurDaft <jacktstupple@gmail.com>
2016-10-22 02:17:37 +0100

    Testing scopes

---
+++ tests/ChannelTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Twitch\Channel;
+
+class ChannelTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function test()
+    {
+        
+    }
+}
